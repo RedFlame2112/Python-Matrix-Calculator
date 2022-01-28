2f26b5987a7765141fe4447ff5728c84120d0df4
RedFlame2112 <90807549+RedFlame2112@users.noreply.github.com>
2022-01-28 09:11:51 -0600

    updates pushed.

--- main.py
+++ main.py
@@ -28,7 +28,8 @@ menu_options = {
     6: 'Add 2 matrices',
     7: 'Subtract 2 matrices',
     8: 'Multiply 2 matrices',
-    9: 'Exit',
+    9: 'Get Eigenvalues and Eigenvectors',
+    10: 'Exit',
 }
 
 
@@ -62,6 +63,8 @@ while True:
     elif option == 8:
         options.option8()
     elif option == 9:
+        options.option9()
+    elif option == 10:
         print('Thank you for using this matrix calculator. Have a good one!')
         exit()
     else:
